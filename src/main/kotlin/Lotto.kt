b2c52ce5a957d35bcac653530a43c2101b1aeffe
leeugun123 <leeugun123@naver.com>
2023-11-06 16:19:33 +0900

    - 로또 뽑기 구현

--- src/main/kotlin/Lotto.kt
+++ src/main/kotlin/Lotto.kt
@@ -6,5 +6,14 @@ class Lotto ( private val numbers: List<Int>) {
         require(numbers.size == 6)
     }
 
+    //추가 메소드 구현
+
+    fun printNumbers(){
+
+        val formattedString = numbers.joinToString(prefix = "[", postfix = "]") { it.toString() }
+        println(formattedString)
+
+    }
+
 
 }
--- src/main/kotlin/LottoUi.kt
+++ src/main/kotlin/LottoUi.kt
@@ -1,21 +1,25 @@
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 class LottoUi {
-
 
     //로또 구입 시작
     fun lottoExc(){
 
+        val purchaseNum = lottoPurchase()
+        println("$purchaseNum"+"개를 구매했습니다.")
 
-        lottoPurchase()
 
+        lottoDraw(purchaseNum)
+        //로또 뽑기
 
+        val drawLottoNum = inputLottoNum()
 
 
 
     }
 
-    private fun lottoPurchase(){
+    private fun lottoPurchase() : Int{
 
         while(true){
 
@@ -25,7 +29,7 @@ class LottoUi {
                 val inputMoney = Console.readLine()
                 CheckData().checkInputMoney(inputMoney)
 
-                break
+                return Integer.parseInt(inputMoney) / 1000
             }
             catch (e : IllegalArgumentException){
                 println("예외 발생: ${e.message} 다시 입력해주세요.")
@@ -34,6 +38,39 @@ class LottoUi {
 
         }
 
+
+    }
+
+    private fun lottoDraw(purchaseNum : Int) {
+
+        repeat(purchaseNum){
+
+
+            val lottoNum = Randoms.pickUniqueNumbersInRange(1,45,6)
+
+            Lotto(lottoNum).printNumbers()
+
+            //당첨된 로또 번호 출력
+
+        }//구매한 로또 개수 만큼 반복
+
+
+    }
+
+    private fun inputLottoNum() : List<Int>{
+
+
+        val tempLotto = mutableListOf<Int>()
+
+        println("당첨 번호를 입력해 주세요.")
+
+        val inputNum = Console.readLine()
+
+        println("보너스 번호를 입력해 주세요.")
+
+        val bonusNum = Console.readLine()
+
+        return tempLotto
 
     }
 
