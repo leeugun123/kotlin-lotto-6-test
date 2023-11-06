a7ce0e783b2db3707b5b904a09aadcd31edff1c6
leeugun123 <leeugun123@naver.com>
2023-11-06 15:53:07 +0900

    - 로또 구매 입력 및 예외처리 수정

--- src/main/kotlin/CheckData.kt
+++ src/main/kotlin/CheckData.kt
@@ -2,22 +2,24 @@ class CheckData {
 
     fun checkInputMoney(inputMoney : String){
 
-        if(!checkDigit(inputMoney))
+        if(checkPrice(inputMoney))
+            throw IllegalArgumentException("1000원 밑으로는 구매 할 수 없습니다.")
+
+        if(checkDigit(inputMoney))
             throw IllegalArgumentException("문자열에 숫자 이외의 문자가 포함되어 있습니다.")
 
-        if(!checkDivide(inputMoney))
+        if(checkDivide(inputMoney))
             throw IllegalArgumentException("1000원 단위로 떨어지지 않습니다.")
 
-        if(!checkPrice(inputMoney))
-            throw IllegalArgumentException("1000원 밑으로는 구매 할 수 없습니다.")
+
 
     }
 
-    private fun checkDigit(inputMoney : String) = inputMoney.all { it.isDigit() }
+    private fun checkDigit(inputMoney : String) = !inputMoney.all { it.isDigit() }
 
-    private fun checkDivide(inputMoney : String) =  Integer.parseInt(inputMoney) % 1000 == 0
+    private fun checkDivide(inputMoney : String) =  Integer.parseInt(inputMoney) % 1000 != 0
 
-    private fun checkPrice(inputMoney : String) = Integer.parseInt(inputMoney) >= 1000
+    private fun checkPrice(inputMoney : String) = Integer.parseInt(inputMoney) < 1000
 
 
 }
